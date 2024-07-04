a6a2e9b41d683b96a4934252e5cc5191788b75f2
Dmitriy <dmitriy.palchikov@gmail.com>
2024-07-04 11:52:00 -0500

    Были произведены изменения по перемещению заголовка

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // Additional JavaScript can be added here if needed in the future.
-});
+function debounce(func, wait) {
+  let timeout;
+  return function () {
+    const context = this;
+    const args = arguments;
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      func.apply(context, args);
+    }, wait);
+  };
+}
+
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+function draw(event) {
+  const x = event.clientX;
+  const y = event.clientY;
+  context.fillStyle = "red";
+  context.beginPath();
+  context.arc(x, y, 5, 0, Math.PI * 2, true);
+  context.fill();
+}
+
+canvas.addEventListener("mousemove", debounce(draw, 10000)); // 100ms задержка
