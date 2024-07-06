8ede058c2541ceccb098167eb6f3d44a249f1b89
Dmitriy <dmitriy.palchikov@gmail.com>
2024-07-06 11:03:58 -0500

    скрипт перенесен в фаил скрипт,изменено позиционирование контактной информации

--- script.js
+++ script.js
@@ -1,25 +1,67 @@
-function debounce(func, wait) {
-  let timeout;
-  return function () {
-    const context = this;
-    const args = arguments;
-    clearTimeout(timeout);
-    timeout = setTimeout(function () {
-      func.apply(context, args);
-    }, wait);
-  };
+let screens = Array.from(document.getElementsByClassName("scrin"));
+let numberScreens = screens.length;
+let web = document.getElementById("web");
+let currentScreens = 0;
+let headTitle = document.getElementById("headTitle");
+let nav = document.getElementById("inf1");
+
+function move(num) {
+  currentScreens = num;
+  web.style.left = -currentScreens * 100 + "vw";
+  web.style.transition = 0.4 + "s";
 }
 
-const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2d");
-
-function draw(event) {
-  const x = event.clientX;
-  const y = event.clientY;
-  context.fillStyle = "red";
-  context.beginPath();
-  context.arc(x, y, 5, 0, Math.PI * 2, true);
-  context.fill();
+function updateHeadTitle() {
+  if (currentScreens === 0) {
+    headTitle.style.top = "45%";
+    headTitle.style.transition = "0.2s";
+  } else {
+    headTitle.style.top = "0%";
+  }
 }
 
-canvas.addEventListener("mousemove", debounce(draw, 10000)); // 100ms задержка
+window.addEventListener("wheel", function (event) {
+  if (event.deltaY > 0) {
+    if (currentScreens < numberScreens) {
+      currentScreens++;
+      web.style.left = -currentScreens * 100 + "vw";
+      headTitle.style.transition = 0.2 + "s";
+      headTitle.style.top = "0%";
+    }
+  } else if (event.deltaY < 0) {
+    if (currentScreens > 0) {
+      currentScreens--;
+      web.style.left = -currentScreens * 100 + "vw";
+      headTitle.style.transition = 0.2 + "s";
+      headTitle.style.top = "0%";
+    }
+  }
+  if (currentScreens == 0) {
+    headTitle.style.transition = 0.2 + "s";
+    headTitle.style.top = "45%";
+  }
+});
+
+document.getElementById("homeBtn").addEventListener("click", function () {
+  currentScreens = 0;
+  web.style.left = -currentScreens * 100 + "vw";
+  updateHeadTitle();
+});
+
+document.getElementById("aboutBtn").addEventListener("click", function () {
+  currentScreens = 1;
+  web.style.left = -currentScreens * 100 + "vw";
+  updateHeadTitle();
+});
+
+document.getElementById("portfolioBtn").addEventListener("click", function () {
+  currentScreens = 2;
+  web.style.left = -currentScreens * 100 + "vw";
+  updateHeadTitle();
+});
+
+document.getElementById("contactBtn").addEventListener("click", function () {
+  currentScreens = 3;
+  web.style.left = -currentScreens * 100 + "vw";
+  updateHeadTitle();
+});
