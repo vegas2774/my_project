989b356bcd1309c10bee40ee387e2207fa0abf84
Dmytro Palchykov <dmitriy.palchikov@gmail.com>
2024-12-14 23:02:42 -0600

    доработал элементы меню и подвязал к скринам, убрал остаточное явления меню на главном экране,доработал CSS HTML and JS для коректной работы

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 //////////////////////////// DECLARATION OF VARIABLES //////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////
 
-
 let screens = Array.from(document.getElementsByClassName("scrin"));
 let numberScreens = screens.length;
 let web = document.getElementById("web");
@@ -27,18 +26,16 @@ for (let i = 0; i < 4; i++)
   ];
 let headerSide = document.getElementsByClassName("header-side")[0];
 let homeIcon = document.querySelector("div.cont-home-icon");
+let newHomeIcon = document.getElementById("cont-home-icon");
 let searchPanel = document.getElementById("search");
 let searchInput = document.getElementById("search-input");
 let ulSearchList = document.getElementById("search-list");
 let headerTitle = Array.from(document.getElementsByClassName("header-title"));
-
 //const bodyBcgSize = parseFloat(window.getComputedStyle(body).backgroundSize);
-
 
 //////////////////////////////////////////////////////////////////////
 //////////////////////////// HEADER //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
 
 // Анимации для выплывания подмкеню при наведении на элементы меню
 
@@ -79,58 +76,77 @@ menu.forEach((menuItem, index) => {
 //Анимация выпадания меню при свернутом header
 
 homeIcon.addEventListener("mouseenter", () => {
-  homeIcon.style.background = "linear-gradient(0deg, rgba(255,255,255,0.22) 64%, rgba(218,37,1,0) 83%)";
+  homeIcon.style.background =
+    "linear-gradient(0deg, rgba(255,255,255,0.22) 64%, rgba(218,37,1,0) 83%)";
   homeIcon.style.height = "460px";
 });
 
 homeIcon.addEventListener("mouseleave", () => {
-  setTimeout(() => {homeIcon.style.background = "rgba(0, 0, 0, 0)";}, 600);
+  setTimeout(() => {
+    homeIcon.style.background = "rgba(0, 0, 0, 0)";
+  }, 600);
   homeIcon.style.height = "75px";
 });
 
 function foldingheader(currentScreen) {
-    if (currentScreen != 0) {
-      headerTitle[1].style.width = "0px";
-      headerTitle[1].style.margin = "0 0 0 0";
-      headerTitle[3].style.width = "0px";
-    } else {
-      headerTitle[1].style.width = "auto";
-      headerTitle[1].style.margin = "0 20px 0 0";
-      headerTitle[3].style.width = "auto";
-    }
-    headerTitle.forEach(function(title) {console.log(title.textContent);});
+  if (currentScreen != 0) {
+    headerTitle[1].style.width = "0px";
+    headerTitle[1].style.margin = "0 0 0 0";
+    headerTitle[3].style.width = "0px";
+  } else {
+    headerTitle[1].style.width = "auto";
+    headerTitle[1].style.margin = "0 20px 0 0";
+    headerTitle[3].style.width = "auto";
+  }
+  headerTitle.forEach(function (title) {
+    console.log(title.textContent);
+  });
 }
 
 function updateheader() {
   if (currentScreens === 0) {
-    header.style.height = "400px";
+    header.style.height = "445px";
     headerSide.style.left = "0px";
     header.style.top = "45%";
     homeIcon.style.top = "-170px";
+    homeIcon.style.display = "none";
     searchPanel.style.top = "-220px";
   } else {
     header.style.height = "90px";
     headerSide.style.left = "-510px";
     homeIcon.style.top = "20px";
+    homeIcon.style.display = "flex";
     header.style.top = "0%";
     searchPanel.style.top = "20px";
   }
 }
 
-
 //////////////////////////////////////////////////////////////////////
 //////////////////////////// SLIDER //////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
 
 // Функция для скринов горизонтальной развертки
 function move(num) {
   currentScreens = num;
   web.style.left = -currentScreens * 100 + "vw";
   web.style.transition = 0.4 + "s";
+  updateheader();
+}
+
+////////////////////// функция для работы с меню когда текущий скрин равен больше 1///////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function showsubmenu(num) {
+  if (currentScreens > 1) {
+    submenuCircle[num].forEach((submenuItem) => {
+      submenuItem.style.opacity = "1";
+      submenuItem.style.transform = "translate(0, 0)";
+    });
+  }
 }
 
 // добавили скрины по горизонтали (прокрутка колесом)
+
 window.addEventListener("wheel", function (event) {
   if (event.deltaY > 0) {
     if (currentScreens < numberScreens) {
@@ -139,7 +155,6 @@ window.addEventListener("wheel", function (event) {
       updateheader();
       body.style.backgroundPosition =
         (100 * currentScreens) / numberScreens + "%";
-      //console.log(100 * currentScreens / (numberScreens) + "%");
     }
   } else if (event.deltaY < 0) {
     if (currentScreens > 0) {
@@ -150,50 +165,63 @@ window.addEventListener("wheel", function (event) {
         (100 * currentScreens) / numberScreens + "%";
     }
   }
-  if (currentScreens == 0) {
+  if (currentScreens === 0) {
     updateheader();
   }
   foldingheader(currentScreens);
 });
 
-//прокрутка скринов по горизонтали (при помощи нажатия кнопки на главной панели)
+////////////////////////////////// работа с сабменю///////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////
 
+submenuCircle.forEach((submenuGroup, screenIndex) => {
+  submenuGroup.forEach((submenuItem) => {
+    submenuItem.addEventListener("click", () => {
+      currentScreens = screenIndex + 1;
+      web.style.left = -currentScreens * 100 + "vw";
+      web.style.transition = "0.4s";
+      updateheader();
+    });
+  });
+});
+///////////////////////////////////// конец раздела сабменю/////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////
 
-homeIcon.addEventListener("click", function () {
+////////////////////// привязка главного меню на домашней странице к скринам//////////////////
+////////////////////////////////////////////////////////////////////////////////////////
+document.getElementById("homeBtn").addEventListener("click", function () {
   currentScreens = 0;
   web.style.left = -currentScreens * 100 + "vw";
   updateheader();
 });
 
-document.getElementById("homeBtn").addEventListener("click", function () {
+document.getElementById("htmlBtn").addEventListener("click", function () {
   currentScreens = 1;
   web.style.left = -currentScreens * 100 + "vw";
   updateheader();
 });
 
-document.getElementById("aboutBtn").addEventListener("click", function () {
+document.getElementById("cssBtn").addEventListener("click", function () {
   currentScreens = 2;
   web.style.left = -currentScreens * 100 + "vw";
   updateheader();
 });
 
-document.getElementById("portfolioBtn").addEventListener("click", function () {
+document.getElementById("jsBtn").addEventListener("click", function () {
   currentScreens = 3;
   web.style.left = -currentScreens * 100 + "vw";
   updateheader();
 });
 
-document.getElementById("contactBtn").addEventListener("click", function () {
+document.getElementById("otherBtn").addEventListener("click", function () {
   currentScreens = 4;
   web.style.left = -currentScreens * 100 + "vw";
   updateheader();
 });
 
-
 //////////////////////////////////////////////////////////////////////
 ////////////////////// POP-UP MODAL-WINDOWS //////////////////////////
 //////////////////////////////////////////////////////////////////////
-
 
 let explanationJS = Array.from(
   document.getElementsByClassName("explanationJS")
@@ -259,99 +287,104 @@ function linkProj(numProj) {
   }
 }
 
-
 //////////////////////////////////////////////////////////////////////
 //////////////////////////// SEARCH /////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-let example = [{
-  title: "HTML",
-  slide: 1,
-  section: 1,
-  ancor: 0
-}, {
-  title: "CSS",
-  slide: 2,
-  section: 1,
-  ancor: 0
-}, {
-  title: "JavaScript",
-  slide: 3,
-  section: 1,
-  ancor: 0
-},
-{
-  title: "FrontEnd",
-  slide: 4,
-  section: 1,
-  ancor: 0
-},
-{
-  title: "HTML basic",
-  slide: 1,
-  section: 1,
-  ancor: 0
-}, {
-  title: "CSS selector",
-  slide: 2,
-  section: 1,
-  ancor: 0
-}, {
-  title: "JavaScript jQuery",
-  slide: 3,
-  section: 1,
-  ancor: 0
-},
-{
-  title: "FrontEnd docer",
-  slide: 4,
-  section: 1,
-  ancor: 0
-},
-{
-  title: "HTML list",
-  slide: 1,
-  section: 1,
-  ancor: 0
-}, {
-  title: "CSS flex",
-  slide: 2,
-  section: 1,
-  ancor: 0
-}, {
-  title: "JavaScript Node.js",
-  slide: 3,
-  section: 1,
-  ancor: 0
-},
-{
-  title: "FrontEnd MySQL",
-  slide: 4,
-  section: 1,
-  ancor: 0
-}
+let example = [
+  {
+    title: "HTML",
+    slide: 1,
+    section: 1,
+    ancor: 0,
+  },
+  {
+    title: "CSS",
+    slide: 2,
+    section: 1,
+    ancor: 0,
+  },
+  {
+    title: "JavaScript",
+    slide: 3,
+    section: 1,
+    ancor: 0,
+  },
+  {
+    title: "FrontEnd",
+    slide: 4,
+    section: 1,
+    ancor: 0,
+  },
+  {
+    title: "HTML basic",
+    slide: 1,
+    section: 1,
+    ancor: 0,
+  },
+  {
+    title: "CSS selector",
+    slide: 2,
+    section: 1,
+    ancor: 0,
+  },
+  {
+    title: "JavaScript jQuery",
+    slide: 3,
+    section: 1,
+    ancor: 0,
+  },
+  {
+    title: "FrontEnd docer",
+    slide: 4,
+    section: 1,
+    ancor: 0,
+  },
+  {
+    title: "HTML list",
+    slide: 1,
+    section: 1,
+    ancor: 0,
+  },
+  {
+    title: "CSS flex",
+    slide: 2,
+    section: 1,
+    ancor: 0,
+  },
+  {
+    title: "JavaScript Node.js",
+    slide: 3,
+    section: 1,
+    ancor: 0,
+  },
+  {
+    title: "FrontEnd MySQL",
+    slide: 4,
+    section: 1,
+    ancor: 0,
+  },
 ];
 
 searchInput.addEventListener("input", function () {
   ulSearchList.innerHTML = "";
-  if(searchInput.value.length >= 2){
-  let seekResult = example.filter(function (listPunct) {
-     return listPunct.title.toLowerCase().includes(searchInput.value);
-  });
-  console.log(seekResult);
-  if (seekResult.length != 0) ulSearchList.style.display = "block";
-  else ulSearchList.style.display = "none";
+  if (searchInput.value.length >= 2) {
+    let seekResult = example.filter(function (listPunct) {
+      return listPunct.title.toLowerCase().includes(searchInput.value);
+    });
+    console.log(seekResult);
+    if (seekResult.length != 0) ulSearchList.style.display = "block";
+    else ulSearchList.style.display = "none";
 
-  for (let i = 0; i < seekResult.length; i++) {
-      let newLi = document.createElement('li');
+    for (let i = 0; i < seekResult.length; i++) {
+      let newLi = document.createElement("li");
       newLi.addEventListener("click", function () {
-          move(i+1);
+        move(i + 1);
       });
       ulSearchList.appendChild(newLi);
       newLi.textContent = seekResult[i].title;
-      };
-  }
-  else {
-      ulSearchList.style.display = "none";
+    }
+  } else {
+    ulSearchList.style.display = "none";
   }
 });
